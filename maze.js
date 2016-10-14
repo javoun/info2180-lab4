2b5ec4fa5d09e983ac42b317e9d255cf36cf7d6a
javoun <javounlong@yahoo.com>
2016-10-14 16:44:50 +0000

    Exercise 1: Single boundary turns red

--- maze.js
+++ maze.js
@@ -1,9 +1,9 @@
 window.onload=function(){
    var topleft=document.getElementById("boundary1");
    topleft.addEventListener("mouseover", function(){
-      topleft.style.borderColor="red";
-      topleft.style.borderWidth="2px";
-     //topleft.setAttribute("class", "div.youlose");
+      topleft.style.borderBottomColor= "red";
+      topleft.style.borderRightColor= "red";
+      //topleft.style.borderWidth="2px";
    });
     
 };
