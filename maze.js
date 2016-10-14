1fb80d1466ab820aadced32cfecbe5d5ce980165
javoun <javounlong@yahoo.com>
2016-10-14 22:01:21 +0000

    Exercuse 2: All boundaries glow red on hover

--- maze.js
+++ maze.js
@@ -1,9 +1,66 @@
-window.onload=function(){
-   var topleft=document.getElementById("boundary1");
-   topleft.addEventListener("mouseover", function(){
-      topleft.style.borderBottomColor= "red";
-      topleft.style.borderRightColor= "red";
-      //topleft.style.borderWidth="2px";
-   });
-    
+window.onload = function() {
+firstBoundary();
+        
+        var allB = document.getElementsByClassName("boundary");
+        for (var i = 0; i < allB.length; i++) {
+        allB[i].setAttribute("class", "boundary")
+        }
+
 };
+
+function firstBoundary() {
+
+var wall = document.getElementById("boundary1");
+
+var allB = document.getElementsByClassName("boundary");
+
+wall.addEventListener("mouseover", function(){
+   // topleft.style.borderBottomColor= "red";
+   // topleft.style.borderRightColor= "red";
+redBoundary();
+});
+
+
+allB[1].addEventListener("mouseover", function(){
+    // topleft.style.borderBottomColor= "red";
+   // topleft.style.borderRightColor= "red";
+         redBoundary();
+   
+});
+
+allB[2].addEventListener("mouseover", function(){
+    // topleft.style.borderBottomColor= "red";
+   // topleft.style.borderRightColor= "red";
+        redBoundary();
+});
+
+allB[3].addEventListener("mouseover", function(){
+    // topleft.style.borderBottomColor= "red";
+   // topleft.style.borderRightColor= "red";
+        redBoundary();
+});
+
+allB[4].addEventListener("mouseover", function(){
+    // topleft.style.borderBottomColor= "red";
+   // topleft.style.borderRightColor= "red";
+    redBoundary();
+});
+};
+
+function redBoundary() {
+    
+    var allB = document.getElementsByClassName("boundary");
+
+    for (var i = 0; i < allB.length; i++) {
+    allB[i].setAttribute("class", "boundary youlose");
+    }
+}
+
+
+
+
+    
+    
+
+
+
