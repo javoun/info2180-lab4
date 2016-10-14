4109f131c7f5df4167124c71f217b637ffc91b44
javoun <javounlong@yahoo.com>
2016-10-14 16:35:47 +0000

    Initial Commit

---
+++ maze.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+   var topleft=document.getElementById("boundary1");
+   topleft.addEventListener("mouseover", function(){
+      topleft.style.borderColor="red";
+      topleft.style.borderWidth="2px";
+     //topleft.setAttribute("class", "div.youlose");
+   });
+    
+};
